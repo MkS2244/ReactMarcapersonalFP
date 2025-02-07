39c4cda29dbe712dab546978499a2f08d08079d1
MkS2244 <13161523@alu.murciaeduca.es>
2025-02-07 11:25:37 +0100

    Creacion de carpetas y componentes

---
+++ src/components/header/Cabecera.jsx
@@ -0,0 +1,15 @@
+import logo from '../../img/mp-logoNaranja100.png'
+import es from '../../img/flag-for-flag-spain-svgrepo-com.svg'
+import en from '../../img/united-kingdom-uk-svgrepo-com.svg'
+
+const Cabecera = () => {
+
+    return (
+        <nav>
+            <img src={logo} alt="Logo Marcapersonal" />
+            <h1>Marca Personal FP</h1>
+            <img src="" alt="" />
+        </nav>
+    )
+}
+export default Cabecera;
