40f7ccbe7378e95454846eadb84ddd7693a59989
MkS2244 <13161523@alu.murciaeduca.es>
2025-02-14 12:50:40 +0100

    Footer en el Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
 
     return (
         <div className='row'>
-            <div className='col-12'>
+            <div className='col-12 space'>
                 <Link to="/empresa" >
                     <img src={empresas} className='imagenes col-12 col-sm-4 ' />
                 </Link>
@@ -31,7 +31,7 @@ const Home = () => {
                     <img src={centros} className='imagenes col-12 col-sm-4' />
                 </Link>
             </div>
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
