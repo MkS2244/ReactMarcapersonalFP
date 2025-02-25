d55338248d1072b4812eb081f3ed0156abca7e7d
MkS2244 <13161523@alu.murciaeduca.es>
2025-02-25 18:57:17 +0100

    .

--- src/components/empresa/Card/Proyectomincard.jsx
+++ src/components/empresa/Card/Proyectomincard.jsx
@@ -1,56 +1,6 @@
-// -------------
-// COMPONENTES
-// -------------
-import useProyects from "../../../hooks/useProyects";
-import avatar from '../../../img/avatar.webp'
 
 const Proyectomincard = (prop) => {
 
-<<<<<<< HEAD
-const Proyectomincard = (prop) => {
-
-    //console.log( "CARD: ", prop)
-
-    function mostrarParticipante (participante)
-    {
-        return <p className="mb-2 pb-1">{participante.nombre}</p>
-    }
-
-    return (
-        <div>
-            <section className="w-100 px-4 py-5 sectionCard">
-                <div className="row d-flex justify-content-center">
-                    <div className="col col-md-9 col-lg-7 col-xl-6">
-                        <div className="card borde">
-
-                            <div className="card-body p-4">
-                                <div className="d-flex">
-                                    <div className="flex-shrink-0">
-                                        <img src="https://source.unsplash.com/random/200x200/?proyecto"
-                                            alt="Generic placeholder image" className="img-fluid imgCard" />
-                                    </div>
-                                    <div className="flex-grow-1 ms-3">
-                                        <h5 className="mb-1">{prop.nombre}</h5>
-                                        <p className="mb-2 pb-1">Alumnos</p>
-                                        {prop.participantes.map(mostrarParticipante)}
-                                        <div className="d-flex justify-content-start rounded-3 p-2 mb-2 bg-body-tertiary">
-                                            <div>
-                                                <p className="small text-muted mb-1">Articles</p>
-                                                <p className="mb-0">41</p>
-                                            </div>
-                                            <div className="px-3">
-                                                <p className="small text-muted mb-1">Followers</p>
-                                                <p className="mb-0">976</p>
-                                            </div>
-                                            <div>
-                                                <p className="small text-muted mb-1">Rating</p>
-                                                <p className="mb-0">8.5</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-=======
-    
 
     return (
         <div>
@@ -67,7 +17,6 @@ const Proyectomincard = (prop) => {
                                 <p className="mb-1">{prop.alumno}</p>
                                 <p className="mb-1"><strong>Tutor:</strong> </p>
                                 <p className="mb-0"><strong>Ciclos:</strong><a href="#">{prop.ciclos.join(" | ")}</a></p>
->>>>>>> origin/main
                             </div>
                         </div>
                     </div>
